ae8576b0de49b3841916953cd33c148f0c8efad0
Raphael Zumer <rzumer@gmail.com>
2016-11-04 19:02:41 -0400

    Implement argument parsing

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strings"
+	"path/filepath"
+)
+
+func main() {
+	// Retrieve command line arguments.
+	args := os.Args[1:]
+	
+	// If not enough arguments are provided, display usage information.
+	if len(args) < 1 || len(args) > 2 {
+		fmt.Printf("Usage: VTT2SRT input [output]\n" +
+			"\n" +
+			"input\tThe input file path.\n" +
+			"output\tThe output file path.\n")
+		
+		return
+	}
+	
+	// Set the input path received as argument.
+	inputPath := args[0]
+	
+	// Set the default output path.
+	outputPath := strings.TrimSuffix(inputPath, filepath.Ext(inputPath)) + ".srt"
+	
+	// If the output path is received as a parameter, set it to the argument.
+	if len(args) == 2 {
+		outputPath = args[1]
+	}
+	
+	// Ensure that the file paths do not collide.
+	if inputPath == outputPath {
+		outputExt := filepath.Ext(outputPath)
+		outputPath = strings.TrimSuffix(outputPath, outputExt) + "_out" + outputExt
+	}
+	
+	// Processing
+}
